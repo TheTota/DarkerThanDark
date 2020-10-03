1c5d4c98a14e2439252f3fb78b26a60e460d6579
Thomas Cianfarani <thomas.ci83@gmail.com>
2020-10-04 00:02:57 +0200

    Add level 1 and goal loading next level

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -1,16 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Goal : MonoBehaviour
 {
     [SerializeField] private WaveController waveController;
-    
+
     [Header("Rotation")]
     [SerializeField] private float rotationSpeed = 2f;
 
     [Header("Waves")]
     [SerializeField] private float delayBetweenWaves = 2f;
+    [SerializeField] private float wavesRadius = 10f;
     private float lastWaveTime;
 
     private void Start()
@@ -26,7 +28,7 @@ public class Goal : MonoBehaviour
         // Emit waves
         if (Time.time - lastWaveTime >= delayBetweenWaves)
         {
-            waveController.EmitWave(this.transform.position, 10, 4, Color.green);
+            waveController.EmitWave(this.transform.position, wavesRadius, 4, Color.green);
             lastWaveTime = Time.time;
         }
     }
@@ -40,7 +42,21 @@ public class Goal : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             Debug.Log("Level complete");
-            // TODO: Load next level or something
+            LoadNextLevel();
+        }
+    }
+
+    private static void LoadNextLevel()
+    {
+        int currentBuildIndex = SceneManager.GetActiveScene().buildIndex;
+        if (currentBuildIndex + 1 <= SceneManager.sceneCount)
+        {
+            SceneManager.LoadScene(currentBuildIndex + 1);
+        }
+        else
+        {
+            // TODO: handle GG
+            Debug.Log("Game completed!");
         }
     }
 }
