e36710fc76db75c6bfcf5b5d78c491e5a2be7ca4
Thomas Cianfarani <thomas.ci83@gmail.com>
2020-10-10 17:35:41 +0200

    Add line of sight check for vision detection

--- Assets/DronevisionDetection.cs
+++ Assets/DronevisionDetection.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class DronevisionDetection : MonoBehaviour
 {
     [SerializeField] private Drone drone;
+    [Header("Vision")]
+    [SerializeField] private float visionRange = 50f;
     private Transform playerTransform;
 
     // Start is called before the first frame update
@@ -16,14 +18,25 @@ public class DronevisionDetection : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float distanceFromPlayer = Vector3.SqrMagnitude(this.transform.position - this.playerTransform.position);
-        if (distanceFromPlayer < 50f)
+        this.transform.LookAt(playerTransform);
+
+        // Check if player in line of sight and at the right distance
+        RaycastHit hit;
+        if (Physics.Raycast(this.transform.position, this.transform.forward, out hit, visionRange))
         {
-            Debug.DrawLine(this.transform.position, this.playerTransform.position, Color.green);
+            if (hit.transform.CompareTag("Player"))
+            {
+                Debug.DrawLine(this.transform.position, hit.point, Color.green);
+                drone.KillThePlayer();
+            }
+            else
+            {
+                Debug.DrawLine(this.transform.position, hit.point, Color.red);
+            }
         }
         else
         {
-            Debug.DrawLine(this.transform.position, this.playerTransform.position, Color.red);
+            Debug.DrawLine(this.transform.position, playerTransform.position, Color.gray);
         }
     }
 }
